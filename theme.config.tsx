6b694bd307d3d042a6a3b47e4c1bd4bd87509bd0
micmc422 <micmc422@gmail.com>
2023-11-27 00:46:02 +0100

    final

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,7 +20,7 @@ const config: DocsThemeConfig = {
     content: "Une question ou un problème ?",
   },
   project: {
-    link: 'https://codesandbox.io/p/devbox/mongodb-tp-tw3-xlc6vr?file=%2Futil%2Fmongodb.js%3A8%2C1',
+    link: 'https://codesandbox.io/p/github/micmc422/MongoDB-TP-TW3',
     icon: <svg className="feather feather-codesandbox" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" /><polyline points="7.5 4.21 12 6.81 16.5 4.21" /><polyline points="7.5 19.79 7.5 14.6 3 12" /><polyline points="21 12 16.5 14.6 16.5 19.79" /><polyline points="3.27 6.96 12 12.01 20.73 6.96" /><line x1="12" x2="12" y1="22.08" y2="12" /></svg>
   },
   footer: {
